4add3bb463146a228aa0bb174c47f153f725ffe7
hackiftekhar <hack.iftekhar@gmail.com>
2014-03-06 19:04:28 +0530

    Updated framework

--- KeyboardTextFieldDemo/IQKeyBoardManager/IQTitleBarButtonItem.m
+++ KeyboardTextFieldDemo/IQKeyBoardManager/IQTitleBarButtonItem.m
@@ -21,8 +21,11 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
+#import <UIKit/UILabel.h>
+
 #import "IQTitleBarButtonItem.h"
 #import "IQKeyboardManagerConstants.h"
+
 
 @implementation IQTitleBarButtonItem
 
