0d9c467e11021548670298ece112001c81d129c1
hackiftekhar <hack.iftekhar@gmail.com>
2014-03-07 16:32:36 +0530

    Added default animation. Added sharing options
    Former-commit-id: 7f7bc5240f9dc45b13519a12ea00f0edd7de6b4f

--- KeyboardTextFieldDemo/KeyboardTextFieldDemo/ViewController.m
+++ KeyboardTextFieldDemo/KeyboardTextFieldDemo/ViewController.m
@@ -4,6 +4,8 @@
 
 #import "ViewController.h"
 #import "UIView+IQKeyboardToolbar.h"
+#import <Twitter/Twitter.h>
+
 
 @interface ViewController ()
 
@@ -14,7 +16,8 @@
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
+    if (self)
+    {
         // Custom initialization
     }
     return self;
@@ -31,6 +34,56 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+- (IBAction)shareClicked:(UIBarButtonItem *)sender
+{
+    NSString *shareString = @"IQKeyboardManager is really great control for iOS developer to manage keyboard-textField.";
+    UIImage *shareImage = [UIImage imageNamed:@"IQKeyboardManagerScreenshot"];
+    NSURL *youtubeUrl = [NSURL URLWithString:@"http://youtu.be/6nhLw6hju2A"];
+
+    if ((floor(NSFoundationVersionNumber) > NSFoundationVersionNumber_iOS_5_1))
+    {
+        NSArray *activityItems = [NSArray arrayWithObjects:
+                                  youtubeUrl,
+                                  shareString,
+                                  shareImage,
+                                   nil];
+        
+        UIActivityViewController *controller = [[UIActivityViewController alloc] initWithActivityItems:activityItems applicationActivities:nil];
+        NSArray *excludedActivities = @[UIActivityTypePrint,
+                                        UIActivityTypeCopyToPasteboard,
+                                        UIActivityTypeAssignToContact,
+                                        UIActivityTypeSaveToCameraRoll];
+        controller.excludedActivityTypes = excludedActivities;
+        [self presentViewController:controller animated:YES completion:nil];
+    }
+    else
+    {
+    if ([TWTweetComposeViewController canSendTweet])
+        {
+            // Initialize Tweet Compose View Controller
+            TWTweetComposeViewController *vc = [[TWTweetComposeViewController alloc] init];
+            // Settin The Initial Text
+            [vc setInitialText:shareString];
+            
+            [vc addImage:shareImage];
+              // Adding a URL
+            [vc addURL:youtubeUrl];
+            // Setting a Completing Handler
+            [vc setCompletionHandler:^(TWTweetComposeViewControllerResult result) {
+                [self dismissModalViewControllerAnimated:YES];
+            }];
+            // Display Tweet Compose View Controller Modally
+            [self presentViewController:vc animated:YES completion:nil];
+        }
+        else
+        {
+            // Show Alert View When The Application Cannot Send Tweets
+            NSString *message = @"The application cannot send a tweet at the moment. This is because it cannot reach Twitter or you don't have a Twitter account associated with this device.";
+            UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Oops" message:message delegate:nil cancelButtonTitle:@"Dismiss" otherButtonTitles:nil];
+            [alertView show];
+        }
+    }
 }
 
 @end
