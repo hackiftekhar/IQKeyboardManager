f65fdf8483f85e603d69252337d3a875ac418ca0
hackiftekhar <hack.iftekhar@gmail.com>
2014-03-20 15:58:43 +0530

    Moved framework directory at the top
    Former-commit-id: 1502ff1207e1e5950e6e726adbbc6ab2404ecaeb

---
+++ IQKeyboardManager Framework/KeyboardManager.framework/Versions/A/Headers/IQTextView.h
@@ -0,0 +1,49 @@
+//
+//  IQTextView.h
+// https://github.com/hackiftekhar/IQKeyboardManager
+// Copyright (c) 2013-14 Iftekhar Qurashi.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#import <UIKit/UITextView.h>
+#import "IQKeyboardManagerConstants.h"
+
+/*!
+ @author Iftekhar Qurashi
+ 
+ @related hack.iftekhar@gmail.com
+ 
+ @class IQTextView
+ 
+ @abstract UITextView with placeholder support
+ */
+NS_CLASS_AVAILABLE_IOS(7_0) @interface IQTextView : UITextView
+
+/*!
+ @property placeholder
+ 
+ @abstract To set textView's placeholder text. Default is ni.
+ */
+@property(nonatomic,copy)   NSString    *placeholder;
+
+@end
+
+
+
+
