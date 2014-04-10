84d8b3d4dd0bf1b9ccd58870876001656ec9b532
hackiftekhar <hack.iftekhar@gmail.com>
2014-04-10 17:22:49 +0530

    Added UIKeyboardAppearance property
    Former-commit-id: 7cbd81ab8453ebcd7c68479c390383982b0e705a

--- KeyboardTextFieldDemo/IQKeyBoardManager/IQKeyboardManager.h
+++ KeyboardTextFieldDemo/IQKeyBoardManager/IQKeyboardManager.h
@@ -23,7 +23,7 @@
 
 #import <Foundation/NSObject.h>
 #import <CoreGraphics/CGBase.h>
-
+#import <UIKit/UITextInputTraits.h>
 #import "IQKeyboardManagerConstants.h"
 
 @class UIFont;
@@ -117,6 +117,25 @@
     @abstract Adjust textView's frame when it is too big in height. Default is NO.
  */
 @property(nonatomic, assign) BOOL canAdjustTextView;
+
+
+
+
+//Keyboard appearance overriding
+
+/*!
+    @property overrideKeyboardAppearance
+ 
+    @abstract override the keyboardAppearance for all textField/textView. Default is NO.
+ */
+@property(nonatomic, assign) BOOL overrideKeyboardAppearance;
+
+/*!
+    @property keyboardAppearance
+ 
+    @abstract if overrideKeyboardAppearance is YES, then all the textField keyboardAppearance is set using this property.
+ */
+@property(nonatomic, assign) UIKeyboardAppearance keyboardAppearance;
 
 
 
