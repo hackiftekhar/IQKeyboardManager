2b3e85baad825879e4fb76d770b6645a8e342ee7
hackiftekhar <hack.iftekhar@gmail.com>
2014-04-10 17:30:11 +0530

    Framework updated
    Former-commit-id: e7dfbcf1436cc2df702da2fd7c7b21b65870b01c

--- KeyboardTextFieldDemo/KeyboardTextFieldDemo/AppDelegate.m
+++ KeyboardTextFieldDemo/KeyboardTextFieldDemo/AppDelegate.m
@@ -6,14 +6,12 @@
 
 #import "AppDelegate.h"
 
-
 @implementation AppDelegate
 
 @synthesize window = _window;
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-
     return YES;
 }
 
