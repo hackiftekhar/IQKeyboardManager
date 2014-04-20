a2d334e5bbb551345a01525d4fbe6465477793e0
hackiftekhar <hack.iftekhar@gmail.com>
2014-04-20 15:30:33 +0530

    Added UINavigationBar demo
    Former-commit-id: de171b67d577b40d6db638a570ff00649c3b5cf5

---
+++ KeyboardTextFieldDemo/KeyboardTextFieldDemo/NavigationBarViewController.h
@@ -0,0 +1,13 @@
+//
+//  NavigationBarViewController.h
+//  IQKeyboard
+//
+//  Created by Atul on 20/04/14.
+//  Copyright (c) 2014 Canopus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NavigationBarViewController : UIViewController
+
+@end
