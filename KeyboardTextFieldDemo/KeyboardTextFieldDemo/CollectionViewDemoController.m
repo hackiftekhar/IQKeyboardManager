95e7c786173752d4b22c561d3ca4b39ef3ab808f
hackiftekhar <hack.iftekhar@gmail.com>
2014-10-29 21:14:01 +0530

    Added UICollectionView Example

---
+++ KeyboardTextFieldDemo/KeyboardTextFieldDemo/CollectionViewDemoController.m
@@ -0,0 +1,33 @@
+//
+//  CollectionViewDemoController.m
+//  IQKeyboard
+//
+//  Created by Iftekhar on 29/10/14.
+//  Copyright (c) 2014 Iftekhar. All rights reserved.
+//
+
+#import "CollectionViewDemoController.h"
+
+@interface CollectionViewDemoController ()<UICollectionViewDelegate>
+
+@end
+
+@implementation CollectionViewDemoController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
+{
+    return 20;
+}
+
+// The cell that is returned must be retrieved from a call to -dequeueReusableCellWithReuseIdentifier:forIndexPath:
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    return [collectionView dequeueReusableCellWithReuseIdentifier:@"TextFieldCollectionViewCell" forIndexPath:indexPath];
+}
+
+@end
