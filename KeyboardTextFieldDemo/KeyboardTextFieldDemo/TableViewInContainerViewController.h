ce67b90a294bc196781c16e3b76a1e8849a235de
jsambells <jeffrey@speakfeel.ca>
2014-12-05 10:27:02 -0500

    Added example to the demo app.
    Former-commit-id: 4fc7ecbd586cfcfad21999cff6c6b7b8764f3b65

---
+++ KeyboardTextFieldDemo/KeyboardTextFieldDemo/TableViewInContainerViewController.h
@@ -0,0 +1,13 @@
+//
+//  TableViewInContainerViewController.h
+//  IQKeyboard
+//
+//  Created by Jeffrey Sambells on 2014-12-05.
+//  Copyright (c) 2014 Iftekhar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewInContainerViewController : UIViewController <UITableViewDataSource, UITableViewDelegate>
+
+@end
