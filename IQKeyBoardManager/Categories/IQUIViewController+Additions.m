486a7ee9c9b6ecb5bbfb9ba1fc915927de2869fc
hackiftekhar <hack.iftekhar@gmail.com>
2015-06-04 19:11:59 +0530

    Added Initial support for TopLayoutGuide and BottomLayoutGuide with Manual Management. Moved 'UIView+IQKeyboardManagerAdditions' to separate category file
    Former-commit-id: b52ea9225b7edd257cd98c197e66ee644e453b8b

---
+++ IQKeyBoardManager/Categories/IQUIViewController+Additions.m
@@ -0,0 +1,39 @@
+//
+//  IQUIViewController+Additions.m
+// https://github.com/hackiftekhar/IQKeyboardManager
+// Copyright (c) 2013-15 Iftekhar Qurashi.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#import "IQUIViewController+Additions.h"
+#import <objc/runtime.h>
+
+@implementation UIViewController (Additions)
+
+-(void)setIQLayoutGuideConstraint:(NSLayoutConstraint *)IQLayoutGuideConstraint
+{
+    objc_setAssociatedObject(self, @selector(IQLayoutGuideConstraint), IQLayoutGuideConstraint, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+
+-(NSLayoutConstraint *)IQLayoutGuideConstraint
+{
+    return objc_getAssociatedObject(self, @selector(IQLayoutGuideConstraint));
+}
+
+@end
