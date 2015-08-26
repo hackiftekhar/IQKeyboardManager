b3c63c803bce1988935563e2c35fef89f86e601d
hackiftekhar <hack.iftekhar@gmail.com>
2015-08-26 16:01:44 +0530

    Removed private marking on Delegate methods and Notifications methods

---
+++ Demo/Swift_Demo/ViewController/SettingsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.swift
+//  Demo
+//
+//  Created by Iftekhar on 26/08/15.
+//  Copyright (c) 2015 Iftekhar. All rights reserved.
+//
+
+import Cocoa
+
+class SettingsViewController: UIViewController {
+
+}
