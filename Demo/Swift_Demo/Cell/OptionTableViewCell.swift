f5281ee2d58e7488bf95c894b1a65d4a9b4dff7c
hackiftekhar <hack.iftekhar@gmail.com>
2015-08-26 17:00:35 +0530

    Implementing Settings Screen on swift demo

---
+++ Demo/Swift_Demo/Cell/OptionTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  OptionTableViewCell.swift
+//  Demo
+//
+//  Created by Iftekhar on 26/08/15.
+//  Copyright (c) 2015 Iftekhar. All rights reserved.
+//
+
+
+class OptionTableViewCell: UITableViewCell {
+
+    @IBOutlet var labelOption : UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.backgroundColor = UIColor.clearColor()
+    }
+}
