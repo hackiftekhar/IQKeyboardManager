a19d7fd6230052f4f2267e21e21fbb900adced15
hackiftekhar <hack.iftekhar@gmail.com>
2015-09-08 00:49:55 +0530

    Fixed #281

--- IQKeyboardManagerSwift/Categories/IQUIView+Hierarchy.swift
+++ IQKeyboardManagerSwift/Categories/IQUIView+Hierarchy.swift
@@ -132,9 +132,9 @@ public extension UIView {
         while let unwrappedSuperView = superView {
             
             if unwrappedSuperView.isKindOfClass(classType) &&
-                ((InternalClass.UITableViewCellScrollViewClass != nil && unwrappedSuperView.isKindOfClass(InternalClass.UITableViewCellScrollViewClass!) == false) &&
-                    (InternalClass.UITableViewWrapperViewClass != nil && unwrappedSuperView.isKindOfClass(InternalClass.UITableViewWrapperViewClass!) == false) &&
-                    (InternalClass.UIQueuingScrollViewClass != nil && unwrappedSuperView.isKindOfClass(InternalClass.UIQueuingScrollViewClass!) == false)) {
+                ((InternalClass.UITableViewCellScrollViewClass == nil || unwrappedSuperView.isKindOfClass(InternalClass.UITableViewCellScrollViewClass!) == false) &&
+                    (InternalClass.UITableViewWrapperViewClass == nil || unwrappedSuperView.isKindOfClass(InternalClass.UITableViewWrapperViewClass!) == false) &&
+                    (InternalClass.UIQueuingScrollViewClass == nil || unwrappedSuperView.isKindOfClass(InternalClass.UIQueuingScrollViewClass!) == false)) {
                         return superView
             } else {
                 
