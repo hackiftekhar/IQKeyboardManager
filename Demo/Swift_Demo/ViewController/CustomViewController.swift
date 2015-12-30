27478c512e499ab978f4f91fb9527b0800661b06
hackiftekhar <hack.iftekhar@gmail.com>
2015-12-31 02:41:32 +0530

    Fixed #347.
    Former-commit-id: 6a202f988002c2c6710ededdea4cdbe0a1449a01

--- Demo/Swift_Demo/ViewController/CustomViewController.swift
+++ Demo/Swift_Demo/ViewController/CustomViewController.swift
@@ -8,19 +8,23 @@
 
 class CustomViewController : UIViewController {
     
+    private var returnHandler : IQKeyboardReturnKeyHandler!
     @IBOutlet private var switchDisableViewController : UISwitch!
     @IBOutlet private var switchDisableToolbar : UISwitch!
     @IBOutlet private var switchConsiderPreviousNext : UISwitch!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+            returnHandler = IQKeyboardReturnKeyHandler(controller: self)
     }
     
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
-        switchDisableViewController.on = IQKeyboardManager.sharedManager().isDisableInViewControllerClass(CustomViewController)
-        switchDisableToolbar.on = IQKeyboardManager.sharedManager().isDisableToolbarInViewControllerClass(CustomViewController)
-        switchConsiderPreviousNext.on = IQKeyboardManager.sharedManager().isConsiderToolbarPreviousNextInViewClass(CustomSubclassView)
+        switchDisableViewController.on = IQKeyboardManager.sharedManager().disabledInViewControllerClassesString().contains(NSStringFromClass(CustomViewController))
+        switchDisableToolbar.on = IQKeyboardManager.sharedManager().disabledInViewControllerClassesString().contains(NSStringFromClass(CustomViewController))
+
+        switchConsiderPreviousNext.on = IQKeyboardManager.sharedManager().consideredToolbarPreviousNextViewClassesString().contains(NSStringFromClass(CustomViewController))
     }
     
     @IBAction func disableInViewControllerAction(sender: UISwitch) {
