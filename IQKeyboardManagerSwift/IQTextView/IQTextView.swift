8fc7f40a1a7e3a2999691a03edbe91b055c1c033
YuriHan <yurihan@yurihan.net>
2017-05-24 00:55:24 +0900

    [IQTextView] Add placeHolderLabel’s textAlignment to follow attribute of IQTextView’s textAlignment.

--- IQKeyboardManagerSwift/IQTextView/IQTextView.swift
+++ IQKeyboardManagerSwift/IQTextView/IQTextView.swift
@@ -67,6 +67,7 @@ open class IQTextView : UITextView {
                     unwrappedPlaceholderLabel.lineBreakMode = .byWordWrapping
                     unwrappedPlaceholderLabel.numberOfLines = 0
                     unwrappedPlaceholderLabel.font = self.font
+                    unwrappedPlaceholderLabel.textAlignment = self.textAlignment
                     unwrappedPlaceholderLabel.backgroundColor = UIColor.clear
                     unwrappedPlaceholderLabel.textColor = UIColor(white: 0.7, alpha: 1.0)
                     unwrappedPlaceholderLabel.alpha = 0
