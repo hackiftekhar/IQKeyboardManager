7352e36fe8dd1efddc081cb4163a7d0ebca97bca
Iftekhar <hack.iftekhar@gmail.com>
2022-05-04 17:23:48 +0530

    Fixed #1856

--- IQKeyboardManagerSwift/Categories/IQUIViewController+Additions.swift
+++ IQKeyboardManagerSwift/Categories/IQUIViewController+Additions.swift
@@ -24,7 +24,7 @@
 import UIKit
 
 @available(iOSApplicationExtension, unavailable)
-@objc public extension UIViewController {
+@objc extension UIViewController {
 
     private struct AssociatedKeys {
         static var IQLayoutGuideConstraint = "IQLayoutGuideConstraint"
@@ -33,7 +33,7 @@ import UIKit
     /**
      This method is provided to override by viewController's if the library lifts a viewController which you doesn't want to lift . This may happen if you have implemented side menu feature in your app and the library try to lift the side menu controller. Overriding this method in side menu class to return correct controller should fix the problem.
     */
-    func parentIQContainerViewController() -> UIViewController? {
+    open func parentIQContainerViewController() -> UIViewController? {
         return self
     }
 
@@ -43,7 +43,7 @@ import UIKit
      @deprecated    Due to change in core-logic of handling distance between textField and keyboard distance, this layout contraint tweak is no longer needed and things will just work out of the box regardless of constraint pinned with safeArea/layoutGuide/superview
     */
     @available(*, deprecated, message: "Due to change in core-logic of handling distance between textField and keyboard distance, this layout contraint tweak is no longer needed and things will just work out of the box regardless of constraint pinned with safeArea/layoutGuide/superview.")
-    @IBOutlet var IQLayoutGuideConstraint: NSLayoutConstraint? {
+    @IBOutlet public var IQLayoutGuideConstraint: NSLayoutConstraint? {
         get {
             return objc_getAssociatedObject(self, &AssociatedKeys.IQLayoutGuideConstraint) as? NSLayoutConstraint
         }
