38bbe7f0fccfc25b650edf23f3448ffbd7034ea5
Steve Smith <stevy@hey.com>
2024-03-19 16:51:34 +0000

    Update Package.swift to include platform for Xcode 15.2+

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "IQKeyboardManagerSwift",
+    platforms: [
+        .iOS(.v13)
+    ],
     products: [
         .library(name: "IQKeyboardManagerSwift",
                  targets: ["IQKeyboardManagerSwift"])
