8839297acc1a8aa14d3bcd97affa236a2a3dc378
Vladimir <iosriabov@gmail.com>
2024-05-06 16:30:01 +0300

    Add PrivacyInfo file

--- Package.swift
+++ Package.swift
@@ -11,7 +11,8 @@ let package = Package(
    targets: [
        .target(
            name: "IQKeyboardManagerSwift",
-           path: "IQKeyboardManagerSwift"
+           path: "IQKeyboardManagerSwift",
+           resources: [.copy("PrivacyInfo.xcprivacy")]
        )
    ]
 )
