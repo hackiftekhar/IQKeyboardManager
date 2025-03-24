555472fdb20a96127643d48d101ef981a40993b6
hackiftekhar <hack.iftekhar@gmail.com>
2025-03-24 13:55:45 +0530

    - Updated IQKeyboardToolbarManager version in Package.swift

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/hackiftekhar/IQKeyboardNotification.git", from: "1.0.5"),
         .package(url: "https://github.com/hackiftekhar/IQTextInputViewNotification.git", from: "1.0.8"),
-        .package(url: "https://github.com/hackiftekhar/IQKeyboardToolbarManager.git", from: "1.1.1"),
+        .package(url: "https://github.com/hackiftekhar/IQKeyboardToolbarManager.git", from: "1.1.3"),
         .package(url: "https://github.com/hackiftekhar/IQKeyboardReturnManager.git", from: "1.0.5"),
         .package(url: "https://github.com/hackiftekhar/IQTextView.git", from: "1.0.5")
     ],
