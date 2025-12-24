5e15518eaa4966527140aeeae96dc306f96cf7b8
hackiftekhar <hack.iftekhar@gmail.com>
2025-12-24 19:13:00 +0530

    Bump 8.0.2

--- Package.swift
+++ Package.swift
@@ -14,10 +14,10 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/hackiftekhar/IQKeyboardNotification.git", from: "1.0.5"),
-        .package(url: "https://github.com/hackiftekhar/IQTextInputViewNotification.git", from: "1.0.8"),
-        .package(url: "https://github.com/hackiftekhar/IQKeyboardToolbarManager.git", from: "1.1.3"),
-        .package(url: "https://github.com/hackiftekhar/IQKeyboardReturnManager.git", from: "1.0.5"),
+        .package(url: "https://github.com/hackiftekhar/IQKeyboardNotification.git", from: "1.0.6"),
+        .package(url: "https://github.com/hackiftekhar/IQTextInputViewNotification.git", from: "1.0.9"),
+        .package(url: "https://github.com/hackiftekhar/IQKeyboardToolbarManager.git", from: "1.1.4"),
+        .package(url: "https://github.com/hackiftekhar/IQKeyboardReturnManager.git", from: "1.0.6"),
         .package(url: "https://github.com/hackiftekhar/IQTextView.git", from: "1.0.5")
     ],
     targets: [
